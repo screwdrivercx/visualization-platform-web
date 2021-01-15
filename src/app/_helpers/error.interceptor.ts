6469975d17b53f886ebaa2d8b832cdfab7d8e56c
screwdrivercx <47163772+screwdrivercx@users.noreply.github.com>
2021-01-15 13:07:56 +0700

    authentication
    - authentication (login,logout,rbac)
    - admin (get,update,delete user)

---
+++ src/app/_helpers/error.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AccountService, AlertService} from '../_services';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private accountService: AccountService,private alertService: AlertService) {}
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            if ([401, 403].includes(err.status) && this.accountService.userValue) {
+                // auto logout if 401 or 403 response returned from api
+
+                this.accountService.logout();
+                this.alertService.error('Unauthorized', { keepAfterRouteChange: true ,autoClose : true});
+            }
+
+            const error = err.error?.message || err.statusText;
+            console.error(err);
+            return throwError(error);
+        }))
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { AccountService } from './_services';
+import { User } from './_models/user';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'visualization-platform-web';
+    title = "visualization-platform";
+    user: User;
+
+    constructor(private accountService: AccountService) {
+        this.accountService.user.subscribe(x => this.user = x);
+    }
+
+    logout() {
+        this.accountService.logout();
+    }
 }
